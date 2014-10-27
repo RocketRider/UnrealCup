982bc70be98cfec6800e5ad4d7a9157ea6506b9c
Daniel <daenewow@freenet.de>
2014-10-27 21:01:51 +0100

    Player sensing enhanced.. added second collision box for kick

--- UnrealCup/script_Player1.lua
+++ UnrealCup/script_Player1.lua
@@ -1,11 +1,10 @@
 counter = 0
 function tick(s)
    counter = counter + 1
-   print(counter)
    if (counter % 100) == 0 then
       Rotate(90)
    else
-      MoveForward(10)
+      MoveForward(0.2)
    end
 end
 print("Player 1 loaded!")
