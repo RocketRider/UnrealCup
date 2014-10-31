90a73b41b61dfe71c5b07d116215373f6d49509e
RocketRider <e-mail@rocketrider.eu>
2014-10-31 17:33:47 +0100

    22 Spieler

--- UnrealCup/script_Player1.lua
+++ UnrealCup/script_Player1.lua
@@ -1,11 +1,18 @@
 counter = 0
+data = 55
 function tick(s)
    counter = counter + 1
+   
+   -- TEST
+   for i=10,1,-1 do 
+	data=data+3
+   end
+   ----------------
+   
    if (counter % 100) == 0 then
       Rotate(90)
    else
       MoveForward(0.2)
    end
 end
-print("Player 1 loaded!")
 
--- UnrealCup/script_Player2.lua
+++ UnrealCup/script_Player2.lua
@@ -2,4 +2,3 @@ function tick(s)
 	Rotate(0.5)
 	MoveForward(10)
 end
-print("Player 2 loaded")
