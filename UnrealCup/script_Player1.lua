4ab4237fc84e60da8444d2c26c03d736a0d8962a
RocketRider <e-mail@rocketrider.eu>
2015-02-16 17:52:08 +0100

    Teamfarben Rot und Blau :D

--- UnrealCup/script_Player1.lua
+++ UnrealCup/script_Player1.lua
@@ -13,10 +13,10 @@ function run()
         
         if(math.abs(x-ballX)<150 and math.abs(y-ballY)<150) then
 			--print("Kick")
-           Kick(goal1_x-x, goal1_y-y,0,10000)
+           Kick(goal1_x-x, goal1_y-y,0,100)
         end
 		if not (ballZ == 0 and ballY == 0 and ballX == 0) then
-			--MoveTo(ballX, ballY, 100)
+			MoveTo(ballX, ballY, 100)
         else
 			--print("Run To goal")
             --MoveTo(goal2_x+500,goal2_y)
