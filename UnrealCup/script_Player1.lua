764fb826f8f287e0c62ebd6f26a6fb64d202485f
Daniel <daenewow@freenet.de>
2015-03-12 23:13:34 +0100

    kleingkeiten Projektmappe & LUA-Script WIP
    ACHTUNG: Änderungen an LUA-Script sind noch nicht lauffähig, werde mal
    bei gelegenheit nochmal schauen was falsch läuft

--- UnrealCup/script_Player1.lua
+++ UnrealCup/script_Player1.lua
@@ -2,7 +2,6 @@ runup = false
 counter = 100
 rotation = 0
 function run()
-	
 	goal1_x,goal1_y,goal1_z = GetGoal1Position()
 	goal2_x,goal2_y,goal2_z = GetGoal2Position()
 	--print("Goal1 ".. goal1_x .. ", " .. goal1_y .. ", " .. goal1_z)
@@ -16,13 +15,13 @@ function run()
 		
         if(math.abs(ballX-x)<150 and math.abs(ballY-y)<150) then
 			--print("Kick")
-			ownAbs = math.pow(goal1_x-x, 2)+math.pow(goal1_y-y, 2) - 300
+			ownAbs = math.sqrt(math.pow(goal1_x-x, 2)+math.pow(goal1_y-y, 2) - 300)
 			pass = false
 			pass_x = 0
 			pass_y = 0
 			for i = 1, #player, 1 do
 				if (player[i][1] == 1) then
-					 playerAbs = math.pow(goal1_x-player[i][3], 2)+math.pow(goal1_y -player[i][4], 2)
+					 playerAbs = math.sqrt(math.pow(goal1_x-player[i][3], 2)+math.pow(goal1_y -player[i][4], 2))
 					 if (playerAbs < ownAbs) then
 						ownAbs = playerAbs
 						pass_x = player[i][3] - x
@@ -52,8 +51,6 @@ function run()
 				end
 			end
 			
-			
-			
 			if (pass) then
 				if (stamina > 50) then
 					Kick(pass_x, pass_y,0,100)
@@ -61,25 +58,54 @@ function run()
 			else
 				Kick(goal1_x-x, goal1_y-y,0,10)
 			end
-           
+          
         end
 		if not (ballZ == 0 and ballY == 0 and ballX == 0) then
-			ownAbs = math.pow(x-ballX, 2)+math.pow(y-ballY, 2)
+            -- check wo is owning the ball (nearest player)
+			minAbs = math.sqrt(math.pow(x-ballX, 2)+math.pow(y-ballY, 2))
+            enemyOwned = false
+            for i = 1, #player, 1 do
+                playerAbs = math.sqrt(math.pow(player[i][3]-ballX, 2)+math.pow(player[i][4]-ballY, 2))
+                if (playerAbs < minAbs) then
+                    minAbs = playerAbs
+                    
+                    if (player[i][1] == 1) then
+                        enemyOwned = false
+                    end
+                    if (player[i][1] == 2) then
+                        enemyOwned = true
+                    end
+                end
+            end
+            -- check if I am the closest player in my team
 			run = true
 			for i = 1, #player, 1 do
 				if (player[i][1] == 1) then
-					 playerAbs = math.pow(player[i][3]-ballX, 2)+math.pow(player[i][4]-ballY, 2)
+					 playerAbs = math.sqrt(math.pow(player[i][3]-ballX, 2)+math.pow(player[i][4]-ballY, 2))
 					 if (playerAbs < ownAbs) then
 						run = false
 						break
 					 end
 				 end
 			end
-		
-		
+            
+            -- if I'm the closest player, go to ball
 			if (run) then
 				MoveTo(ballX, ballY, 100)
 			end
+            
+            -- not the closest player: adapt tactic depending on ball owner
+            if not (run) then
+                if (enemyOwned) then
+                    -- move back (defend)
+                    MoveTo(x-100, y-100, 20)
+                end
+                if not (enemyOwned) then
+                    -- move foward (attack)
+                    MoveTo(x+100, y+100, 50)
+                end
+            end
+        end
         else
 			--print("Run To goal")
             --MoveTo(goal2_x+500,goal2_y)
